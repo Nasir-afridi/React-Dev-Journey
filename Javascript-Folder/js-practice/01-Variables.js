7eef28557ef65aab86d3034326fc86495cdebe6d
Nasir-afridi <nasirwc05@gmail.com>
2025-06-30 17:45:25 +0500

    also added the data types + typeof

--- Javascript-Folder/js-practice/01-Variables.js
+++ Javascript-Folder/js-practice/01-Variables.js
@@ -1,3 +1,15 @@
 var name = "Ali"; // function-scoped
 let age = 19;     // block-scoped
 const city = "Wah Cantt"; // block-scoped, can't change
+
+//data types
+let username = "Nasir";    // string
+let ages = 19;              // number
+let isStudent = true;      // boolean
+let score = null;          // null
+let phone;                 // undefined
+
+//check the data type of the variables.
+console.log(typeof username); // string
+console.log(typeof score);    // object (js bug)
+console.log(typeof phone);    // undefined
