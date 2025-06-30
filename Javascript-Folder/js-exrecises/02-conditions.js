01dcfaf17be9ecb031d10667fb294a955faf6d89
Nasir-afridi <nasirwc05@gmail.com>
2025-06-30 18:38:33 +0500

    Exercise of if-else condition in which checking the grading of students

--- Javascript-Folder/js-exrecises/02-conditions.js
+++ Javascript-Folder/js-exrecises/02-conditions.js
@@ -16,3 +16,17 @@ if(age>=18) {
 }else{
     console.log('Minor')
 }
+
+
+
+
+
+
+let marks = 75;
+if(marks >= 80) {
+    console.log('A+ grade')
+}else if(marks >= 60 && marks <= 79) {
+    console.log('B grade')
+}else if (marks <= 59) {
+    console.log('fail')
+}
