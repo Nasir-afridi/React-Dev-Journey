1862cfea2b4f79ef7af56499fe58782fcd44b07a
Nasir-afridi <nasirwc05@gmail.com>
2025-06-30 17:53:50 +0500

    Added the exercise of variables + datatypes

---
+++ Javascript-Folder/js-exrecises/01-variables.js
@@ -0,0 +1,16 @@
+//data types.
+let username = 'nasir';
+let age = 20;
+let male = true;
+let value = null;
+let adress;
+
+//checking the type of variables.
+console.log(typeof(username))
+console.log(typeof(age))
+console.log(typeof(male))
+console.log(typeof(value))
+console.log(typeof(adress))
+
+ username = 'Muhammad';
+ console.log(username);
