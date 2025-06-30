a6e7ce7a7c210d6b3ad57c3d27dde4d5f2b47aeb
Nasir-afridi <nasirwc05@gmail.com>
2025-06-30 18:25:09 +0500

    Added the conditions and operators

---
+++ Javascript-Folder/js-practice/02-conditions.js
@@ -0,0 +1,10 @@
+// Conditions and operator.
+
+let isStudent = true;
+let isLoggedIn = false;
+
+if (isStudent && isLoggedIn) {
+  console.log("Access granted");
+} else {
+  console.log("Access denied");
+}
