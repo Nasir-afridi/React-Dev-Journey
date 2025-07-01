65bba7350caacb9b697a86b947eb7bd0004081c3
Nasir-afridi <nasirwc05@gmail.com>
2025-07-01 19:27:02 +0500

    added the array and methods

---
+++ Javascript-Folder/js-exrecises/05-Array.js
@@ -0,0 +1,9 @@
+let arr = ['red', 'blue', 'green'];
+arr.push('black');
+arr.unshift('yellow');
+arr.pop();
+arr.shift();
+console.log(arr.includes('blue'));
+console.log(arr.indexOf('red'));
+console.log(arr.reverse());
+console.log(arr.join(''));
