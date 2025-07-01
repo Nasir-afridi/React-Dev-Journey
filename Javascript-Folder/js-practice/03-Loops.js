e5c36dc61ebda3f0e2ca6f7a1d00b92dc9766521
Nasir-afridi <nasirwc05@gmail.com>
2025-07-01 12:14:38 +0500

    Add the combination of loops and conditions.

--- Javascript-Folder/js-practice/03-Loops.js
+++ Javascript-Folder/js-practice/03-Loops.js
@@ -18,3 +18,11 @@ do {
   console.log("Do Number:", j);
   j++;
 } while (j <= 3);
+
+
+// loops and condition combine.
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log(i, "is even");
+  }
+}
