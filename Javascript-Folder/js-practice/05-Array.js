705a2081941e579e7b3f93924931eb5186c5c337
Nasir-afridi <nasirwc05@gmail.com>
2025-07-01 19:19:26 +0500

    shift and unshift added

--- Javascript-Folder/js-practice/05-Array.js
+++ Javascript-Folder/js-practice/05-Array.js
@@ -2,3 +2,5 @@ let colors = ['red', 'blue', 'green'];
 console.log(colors[0]);
 colors.push('black'); // ['red', 'blue', 'green', 'black']
 colors.pop(); // removes 'black'
+colors.unshift('yellow'); // ['yellow', 'red', 'blue', 'green']
+colors.shift(); // removes 'yellow'
