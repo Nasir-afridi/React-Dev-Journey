c53c5d5bfee52430b407826e80a518f1279e5b7a
Nasir-afridi <nasirwc05@gmail.com>
2025-07-01 19:21:02 +0500

    includes, indexOf, reverse, join added

--- Javascript-Folder/js-practice/05-Array.js
+++ Javascript-Folder/js-practice/05-Array.js
@@ -4,3 +4,7 @@ colors.push('black'); // ['red', 'blue', 'green', 'black']
 colors.pop(); // removes 'black'
 colors.unshift('yellow'); // ['yellow', 'red', 'blue', 'green']
 colors.shift(); // removes 'yellow'
+console.log(colors.includes('blue')); // true
+console.log(colors.indexOf('red')); // 0
+colors.reverse(); // reverses order
+console.log(colors.join('-')); // red-blue-green
