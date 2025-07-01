525b8030b8a88b0579ff7aab1c93a1839d7788a8
Nasir-afridi <nasirwc05@gmail.com>
2025-07-01 12:01:39 +0500

    added the for loop

---
+++ Javascript-Folder/js-practice/03-Loops.js
@@ -0,0 +1,18 @@
+// for loop.
+for (let i = 1; i <= 5; i++) {
+  console.log("Number:", i);
+}
+
+//while loop.
+let i = 1;
+while (i <= 5) {
+  console.log("While Number:", i);
+  i++;
+}
+
+// do...while loop.
+let j = 1;
+do {
+  console.log("Do Number:", j);
+  j++;
+} while (j <= 3);
