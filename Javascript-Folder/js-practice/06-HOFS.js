f5f9e8c4c8eee2121d392447e006786855ed8b68
Nasir-afridi <nasirwc05@gmail.com>
2025-07-02 16:23:14 +0500

    Added the map which doubled the array

--- Javascript-Folder/js-practice/06-HOFS.js
+++ Javascript-Folder/js-practice/06-HOFS.js
@@ -2,3 +2,9 @@ let arr = [1, 2, 3];
 arr.forEach(function(num) {
   console.log(num);
 });
+
+let nums = [2, 4, 6];
+let doubled = nums.map(function(num) {
+  return num * 2;
+});
+console.log(doubled);
