bc3011b231da7bcb2d903cc1602ec61a5edfca0f
Nasir-afridi <nasirwc05@gmail.com>
2025-07-02 16:50:40 +0500

    added the reduce to sum the array

--- Javascript-Folder/js-exrecises/06-HOFS.js
+++ Javascript-Folder/js-exrecises/06-HOFS.js
@@ -12,3 +12,11 @@ let res = arrs.filter(function(str) {
     return str.length > 3
 })
 console.log(res);
+
+
+let redu = [100, 200, 50]
+let red = redu.reduce(function(acc, curr) {
+    return acc + curr
+})
+
+console.log(red);
