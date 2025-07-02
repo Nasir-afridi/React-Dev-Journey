cff0454aaaa7b76bfa00d718633ab30fd8948ee9
Nasir-afridi <nasirwc05@gmail.com>
2025-07-02 16:34:52 +0500

    use the filter on the array

--- Javascript-Folder/js-exrecises/06-HOFS.js
+++ Javascript-Folder/js-exrecises/06-HOFS.js
@@ -5,3 +5,10 @@ let result = arr.map(function(num) {
 })
 
 console.log(result)
+
+
+let arrs = ['he','nasir', 'ali', 'bilal', 'asif']
+let res = arrs.filter(function(str) {
+    return str.length > 3
+})
+console.log(res);
