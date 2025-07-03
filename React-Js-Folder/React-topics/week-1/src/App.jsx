4ef5ac3364b9780f71f2869321a698555ef045db
Nasir-afridi <nasirwc05@gmail.com>
2025-07-03 17:06:40 +0500

    import the Message component

--- React-Js-Folder/React-topics/week-1/src/App.jsx
+++ React-Js-Folder/React-topics/week-1/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Greeting from "./components/Greeting"
+import Message from "./components/Message"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
      <Header />
      <Footer />
      <Greeting name='Nasir' />
+     <Message />
     </>
   )
 }
