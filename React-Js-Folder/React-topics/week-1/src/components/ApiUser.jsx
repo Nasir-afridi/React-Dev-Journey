49ec93b97c09647233c5ba662eafb617e040af3f
Nasir-afridi <nasirwc05@gmail.com>
2025-07-08 14:43:16 +0500

    fetch the api using the useEffect

---
+++ React-Js-Folder/React-topics/week-1/src/components/ApiUser.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+const ApiUsers = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Users from API</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              <strong>{user.name}</strong> - {user.email}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default ApiUsers;
