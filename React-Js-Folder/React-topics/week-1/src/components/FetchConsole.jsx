7855feadd7a5a3f9d2371de297eba718ac43d154
Nasir-afridi <nasirwc05@gmail.com>
2025-07-14 18:07:25 +0500

    showing only the users name and email on the screen

--- React-Js-Folder/React-topics/week-1/src/components/FetchConsole.jsx
+++ React-Js-Folder/React-topics/week-1/src/components/FetchConsole.jsx
@@ -1,14 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const FetchConsole = () => {
+    const [users, setUsers] = useState([]);
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users")
         .then((res) => res.json())
-        .then((data) => console.log(data))
+        .then((data) => setUsers(data))
     } , [])
   return (
    <>
    <h2>Fetching data...</h2>
+   <ul>
+    
+        {users.map((user) => ( 
+            <li key={user.id}>
+                {user.name} : {user.email}
+            </li>))
+           
+        }
+    
+   </ul>
    </>
   )
 }
