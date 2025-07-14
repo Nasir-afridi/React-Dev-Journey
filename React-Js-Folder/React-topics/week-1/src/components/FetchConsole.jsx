c52683f30719b0430ec3fda1fc6554ad88d1599a
Nasir-afridi <nasirwc05@gmail.com>
2025-07-14 18:57:42 +0500

    added the conditions

--- React-Js-Folder/React-topics/week-1/src/components/FetchConsole.jsx
+++ React-Js-Folder/React-topics/week-1/src/components/FetchConsole.jsx
@@ -1,27 +1,32 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const FetchConsole = () => {
-    const [users, setUsers] = useState([]);
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-        .then((res) => res.json())
-        .then((data) => setUsers(data))
-    } , [])
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      });
+  }, []);
   return (
-   <>
-   <h2>Fetching data...</h2>
-   <ul>
-    
-        {users.map((user) => ( 
+    <>
+      <h2>Fetching data...</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {users.map((user) => (
             <li key={user.id}>
-                {user.name} : {user.email}
-            </li>))
-           
-        }
-    
-   </ul>
-   </>
-  )
-}
+              {user.name} : {user.email}
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
 
 export default FetchConsole;
